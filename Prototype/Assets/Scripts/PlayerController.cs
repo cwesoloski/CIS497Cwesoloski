c69a5ad917dabd37f81b9dd9a9761e57441e3b3c
cwesoloski <70545011+cwesoloski@users.noreply.github.com>
2020-09-02 16:11:14 -0500

    Changed the Scene and added more to the project

---
+++ Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+    private float moveHorizontal;
+    private float moveVertical;
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveVertical);
+
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * moveHorizontal);
+    }
+}
---
+++ Prototype/Assets/Scripts/WinTriggerBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTriggerBox : MonoBehaviour
+{
+    public Text textbox;
+
+    private void OntriggerEnter(Collider other)
+    {
+        
+    }
+}
