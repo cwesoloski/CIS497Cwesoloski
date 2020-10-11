6bdcac8bb0493e75bec91de32c93c4aeb60d5e50
cwesoloski <70545011+cwesoloski@users.noreply.github.com>
2020-10-11 17:38:42 -0500

    Finished Challenge 3

---
+++ Challenge 3/Assets/Scripts/UIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoretext;
+    public PlayerControllerX playerControllerScript;
+    public bool won;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerX>();
+
+        scoretext.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!playerControllerScript.gameOver)
+        {
+            scoretext.text = "Score: " + score;
+        }
+
+        if(playerControllerScript.gameOver && !won)
+        {
+            scoretext.text = "You lose! Press R to try Again!";
+        }
+
+        if(score >= 20)
+        {
+            playerControllerScript.gameOver = true;
+            won = true;
+
+            scoretext.text = "You win! Press R to play Again!";
+        }
+    }
+}
