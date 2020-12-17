593515ae6060f2e52d1f9845972c3e921022ee3b
cwesoloski <70545011+cwesoloski@users.noreply.github.com>
2020-12-17 00:16:05 -0600

    Moved Assinment 6

---
+++ Assignment6/Assets/Scripts/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon: MonoBehaviour
+{
+    public int damageBonus;
+
+    public Enemy enemyHoldingWeapon;
+
+    public void Recharge()
+    {
+        Debug.Log("Recharging weapon");
+    }
+
+    private void Awake()
+    {
+        enemyHoldingWeapon = gameObject.GetComponent<Enemy>();
+        EnemyEatsWeapon(enemyHoldingWeapon);
+    }
+
+    protected void EnemyEatsWeapon(Enemy enemyHolding)
+    {
+        Debug.Log("Enemy Eats weapon");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
