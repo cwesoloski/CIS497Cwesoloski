097cb455fffc93faa730b393b98ab1fdc8903e13
cwesoloski <70545011+cwesoloski@users.noreply.github.com>
2021-09-03 19:59:06 -0500

    Stuff

---
+++ Assignment6/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int score;
+    public static GameManager instance;
+
+    private string currentLevelName;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            Debug.LogError("Trying to instantiate a second");
+        }
+    }
+
+    //Methods to load and unload scenes
+
+    public void LoadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(levelName,LoadSceneMode.Additive);
+
+        if(ao == null)
+        {
+            Debug.LogError("[Gamemanager] Unable to load level " + levelName);
+            return;
+        }
+        currentLevelName = levelName;
+    }
+
+    public void UnloadLevel(string levelName)
+    {
+
+    }
+
+}
